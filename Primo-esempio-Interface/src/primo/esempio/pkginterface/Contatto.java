9a98a037c6fb793d4ff033b25ace2ab2c00f6817
Adel Lis <lisadel0510@gmail.com>
2022-03-25 14:25:06 +0100

    aggiorno file Contatto.java con metodi get(), tostring() e equals()

--- Primo-esempio-Interface/src/primo/esempio/pkginterface/Contatto.java
+++ Primo-esempio-Interface/src/primo/esempio/pkginterface/Contatto.java
@@ -3,40 +3,93 @@ Classe Contatto - Contiene informazioni sui contatti (nome, cognome, telefono, e
  */
 package primo.esempio.pkginterface;
 
+import java.util.Objects;
+
 public class Contatto {
     private String nome;
-    private String congome;
+    private String cognome;
     private String telefono;
     private String email;
 
-    public Contatto(String nome, String congome, String telefono, String email) throws Exception{
-       
-        valida(nome, congome, telefono, email);
-        
+    public Contatto(String nome, String cognome, String telefono, String email) throws Exception {
+        valida(nome,cognome,telefono,email);
         this.nome = nome;
-        this.congome = congome;
+        this.cognome = cognome;
         this.telefono = telefono;
         this.email = email;
     }
+    //facciamo: to string, get, equals
 
-    private void valida(String nome, String congome, String telefono, String email) throws Exception{
-        if (nome == null){
-            throw new NullPointerException("nome null");
+    @Override
+    public String toString() {
+        return "Contatto{" + "nome=" + nome + ", cognome=" + cognome + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + Objects.hashCode(this.nome);
+        hash = 11 * hash + Objects.hashCode(this.cognome);
+        hash = 11 * hash + Objects.hashCode(this.telefono);
+        hash = 11 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
         }
-        if (congome == null){
-            throw new NullPointerException("congome null");
+        if (obj == null) {
+            return false;
         }
-        if (telefono == null){
-            throw new NullPointerException("telefono null");
+        if (getClass() != obj.getClass()) {
+            return false;
         }
-        if (email == null){
-            throw new NullPointerException("email null");
-        }
-        //manca altri controlli. (che ci sia solo una @ nella mail) (numero di telefono sia 11) 
-        
+        final Contatto other = (Contatto) obj;
+        return true;
     }
     
-    //fare get(), equals()
-    //non fare costruttore di copia
-    //fare toString()
+    //non facciamo il costruttore di copia
+    private void valida(String nome, String cognome, String telefono, String email) throws Exception{
+        if(nome==null)
+            throw new NullPointerException("nome null");
+        if(cognome==null)
+            throw new NullPointerException("cognome null");
+        if(telefono==null)
+            throw new NullPointerException("telefono null");
+        if(email==null)
+            throw new NullPointerException("email null");
+        //manca controllo che ci sia almeno una @ nell'email
+        //manca controllo che numero di digit di numero di telefono sia <= 11 e >= non lo so
+
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    /*
+    public void setTelefono(String nuovoTelefono) throws Exception {
+        this.valida(this.nome, this.cognome, nuovoTelefono, this.email);
+        this.telefono=nuovoTelefono;        
+       //this.valida(this.getNome(), this.getCognome(), nuovoTelefono, this.getEmail());
+    }
+    
+    public void setEmail(String e) {
+        
+    }
+    */
 }
